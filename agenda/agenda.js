3a2250136245ef4437cf75a225752b47da86d1cb
wilsonf1996 <152716195+wilsonf1996@users.noreply.github.com>
2023-12-02 20:51:42 -0300

    Update agenda.js

--- agenda/agenda.js
+++ agenda/agenda.js
@@ -1,22 +1,75 @@
-// Seu código JavaScript aqui
-// Import the functions you need from the SDKs you need
+// Importar as funções necessárias do Firebase
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, collection, addDoc, getDocs, query, where, orderBy, deleteDoc, doc, updateDoc } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyAIp-rFuZZsBCNVJ3pSge4TE-XUuwYygrI",
-    authDomain: "agenda-6accc.firebaseapp.com",
-    projectId: "agenda-6accc",
-    storageBucket: "agenda-6accc.appspot.com",
-    messagingSenderId: "794262176773",
-    appId: "1:794262176773:web:9bcd82e5cffd858adc086c",
-    measurementId: "G-QS6WRF6Y6L"
+  // (sua configuração Firebase aqui)
 };
 
-// Initialize Firebase
+// Inicializar o Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Função para criar um novo evento
+async function createEvent(title, start, end) {
+  // Implemente a lógica para criar um novo evento no banco de dados
+  const eventsCollection = collection(db, "events");
+  await addDoc(eventsCollection, { title, start, end });
+}
+
+// Função para obter todos os eventos do banco de dados
+async function getAllEvents() {
+  // Implemente a lógica para obter todos os eventos do banco de dados
+  const eventsCollection = collection(db, "events");
+  const eventsQuery = query(eventsCollection, orderBy("start"));
+  const snapshot = await getDocs(eventsQuery);
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+}
+
+// Função para excluir um evento
+async function deleteEvent(eventId) {
+  // Implemente a lógica para excluir um evento do banco de dados
+  const eventRef = doc(db, "events", eventId);
+  await deleteDoc(eventRef);
+}
+
+// Função para atualizar um evento
+async function updateEvent(eventId, updatedData) {
+  // Implemente a lógica para atualizar um evento no banco de dados
+  const eventRef = doc(db, "events", eventId);
+  await updateDoc(eventRef, updatedData);
+}
+
+// Função para abrir o modal de edição de evento
+function openEditEventModal(eventId) {
+  // Implemente a lógica para abrir o modal de edição com base no eventId
+  const event = // Obtenha os detalhes do evento com base no eventId
+  // Preencha o modal com os detalhes do evento
+  // Abra o modal
+}
+
+// Função para fechar o modal de edição de evento
+function closeModal() {
+  // Implemente a lógica para fechar o modal
+}
+
+// Função para abrir o modal de criação de evento
+function openCreateEventModal() {
+  // Implemente a lógica para abrir o modal de criação
+}
+
+// Função para abrir o modal de pesquisa de eventos
+function openSearchEventModal() {
+  // Implemente a lógica para abrir o modal de pesquisa
+}
+
+// Função para retornar à página principal
+function returnToMainPage() {
+  // Redirecionar para o index ou a página principal desejada
+  window.location.href = "../index.html";
+}
+
+// (Restante do seu código)
