8235ccc4ce369482f9c9f7734df66be2460d5b17
wilsonf1996 <152716195+wilsonf1996@users.noreply.github.com>
2023-12-02 19:37:14 -0300

    Update agenda.js

--- agenda/agenda.js
+++ agenda/agenda.js
@@ -1,47 +1,22 @@
-// Seu código Firebase aqui
+// Seu código JavaScript aqui
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Seu web app's Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAIp-rFuZZsBCNVJ3pSge4TE-XUuwYygrI",
-  authDomain: "agenda-6accc.firebaseapp.com",
-  projectId: "agenda-6accc",
-  storageBucket: "agenda-6accc.appspot.com",
-  messagingSenderId: "794262176773",
-  appId: "1:794262176773:web:9bcd82e5cffd858adc086c",
-  measurementId: "G-QS6WRF6Y6L"
+    apiKey: "AIzaSyAIp-rFuZZsBCNVJ3pSge4TE-XUuwYygrI",
+    authDomain: "agenda-6accc.firebaseapp.com",
+    projectId: "agenda-6accc",
+    storageBucket: "agenda-6accc.appspot.com",
+    messagingSenderId: "794262176773",
+    appId: "1:794262176773:web:9bcd82e5cffd858adc086c",
+    measurementId: "G-QS6WRF6Y6L"
 };
 
-// Inicialize Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-// Inicialize Firestore
-const db = getFirestore(app);
-
-// Inicialize Authentication (se necessário)
-const auth = getAuth(app);
-
-function redirectToIndex() {
-    // Adicione o redirecionamento para o Index aqui
-}
-
-// Funções para manipular o banco de dados Firestore
-async function fetchEvents() {
-    const eventsCollection = collection(db, 'events');
-    const eventsSnapshot = await getDocs(eventsCollection);
-    const events = eventsSnapshot.docs.map(doc => doc.data());
-    return events;
-}
-
-async function addEvent(eventData) {
-    const eventsCollection = collection(db, 'events');
-    const newEventRef = await addDoc(eventsCollection, eventData);
-    return newEventRef.id;
-}
-
-// Adicione outras funções conforme necessário
-
