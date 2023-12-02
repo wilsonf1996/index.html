4daa230a16f76d852d5b79c07c88a4b6968c5720
wilsonf1996 <152716195+wilsonf1996@users.noreply.github.com>
2023-12-02 20:56:31 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
+// index.js
+
+// Função para redirecionar para a página da calculadora
+function goToCalculator() {
+  window.location.href = "calculadora/calculadora.html";
+}
+
+// Função para redirecionar para a página da agenda
+function goToAgenda() {
+  window.location.href = "agenda/agenda.html";
+}
 
