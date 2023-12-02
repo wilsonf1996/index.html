18d812a7f3b5f5a11e270006d6f1c1332003d903
wilsonf1996 <152716195+wilsonf1996@users.noreply.github.com>
2023-12-02 20:12:05 -0300

    Update and rename firebase.js to firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+// Arquivo firebaseConfig.js
+// Substitua com suas próprias configurações do Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyAIp-rFuZZsBCNVJ3pSge4TE-XUuwYygrI",
+  authDomain: "agenda-6accc.firebaseapp.com",
+  projectId: "agenda-6accc",
+  storageBucket: "agenda-6accc.appspot.com",
+  messagingSenderId: "794262176773",
+  appId: "1:794262176773:web:9bcd82e5cffd858adc086c",
+  measurementId: "G-QS6WRF6Y6L",
+};
+
+
