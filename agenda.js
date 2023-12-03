325bacc806ec9f69a794806bff45bedc49018f5f
wilsonf1996 <152716195+wilsonf1996@users.noreply.github.com>
2023-12-02 22:27:10 -0300

    Update agenda.js

--- agenda.js
+++ agenda.js
@@ -1,1 +1,14 @@
+// firebaseConfig.js
+// Configuração do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAIp-rFuZZsBCNVJ3pSge4TE-XUuwYygrI",
+    authDomain: "agenda-6accc.firebaseapp.com",
+    projectId: "agenda-6accc",
+    storageBucket: "agenda-6accc.appspot.com",
+    messagingSenderId: "794262176773",
+    appId: "1:794262176773:web:9bcd82e5cffd858adc086c",
+    measurementId: "G-QS6WRF6Y6L"
+};
+
+export default firebaseConfig;
 
