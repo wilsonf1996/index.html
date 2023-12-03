69560572e31d0d1890372def7ce192207ce0f3a0
wilsonf1996 <152716195+wilsonf1996@users.noreply.github.com>
2023-12-02 21:06:16 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+
+// Função para redirecionar para a página da calculadora
+function goToCalculator() {
+  window.location.href = "calculadora/calculadora.html";
+}
+
+// Função para redirecionar para a página da agenda
+function goToAgenda() {
+  window.location.href = "agenda/agenda.html";
+}
