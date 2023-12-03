00b385ca10fa3aecd3bd6f98d357675ccde7948e
wilsonf1996 <152716195+wilsonf1996@users.noreply.github.com>
2023-12-02 21:51:27 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,12 @@
-// Arquivo firebaseConfig.js
-// Substitua com suas próprias configurações do Firebase
-export const firebaseConfig = {
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
   apiKey: "AIzaSyAIp-rFuZZsBCNVJ3pSge4TE-XUuwYygrI",
   authDomain: "agenda-6accc.firebaseapp.com",
   projectId: "agenda-6accc",
   storageBucket: "agenda-6accc.appspot.com",
   messagingSenderId: "794262176773",
   appId: "1:794262176773:web:9bcd82e5cffd858adc086c",
-  measurementId: "G-QS6WRF6Y6L",
-};
-
-
+  measurementId: "G
