9c94865971ab0547731c2c6b1388594d6fa0cbec
wilsonf1996 <152716195+wilsonf1996@users.noreply.github.com>
2023-12-02 21:56:08 -0300

    Update agenda.js

--- agenda/agenda.js
+++ agenda/agenda.js
@@ -1,75 +1,141 @@
-// Importar as funções necessárias do Firebase
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
-import { getFirestore, collection, addDoc, getDocs, query, where, orderBy, deleteDoc, doc, updateDoc } from "firebase/firestore";
+// agenda.js
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, addDoc, collection, getDocs, query, where, deleteDoc, doc, updateDoc } from "firebase/firestore";
 
-// Configuração do Firebase
-const firebaseConfig = {
-  // (sua configuração Firebase aqui)
+// Verifica se o usuário está autenticado
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+    if (!user) {
+        // Redireciona para a página de login se o usuário não estiver autenticado
+        window.location.href = "login.html";
+    }
+});
+
+// Referência para a coleção de eventos
+const eventsCollection = collection(getFirestore(), "events");
+
+// Função para criar um evento
+const createEvent = async (title, start, end) => {
+    try {
+        await addDoc(eventsCollection, {
+            title: title,
+            start: start,
+            end: end,
+            userId: auth.currentUser.uid,
+        });
+        alert("Evento criado com sucesso!");
+        closeModal();
+    } catch (error) {
+        console.error("Erro ao criar evento: ", error);
+    }
 };
 
-// Inicializar o Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+// Função para buscar eventos do usuário autenticado
+const getEvents = async () => {
+    const q = query(eventsCollection, where("userId", "==", auth.currentUser.uid));
+    const querySnapshot = await getDocs(q);
+    const events = [];
+    querySnapshot.forEach((doc) => {
+        events.push({ id: doc.id, ...doc.data() });
+    });
+    return events;
+};
 
-// Função para criar um novo evento
-async function createEvent(title, start, end) {
-  // Implemente a lógica para criar um novo evento no banco de dados
-  const eventsCollection = collection(db, "events");
-  await addDoc(eventsCollection, { title, start, end });
-}
-
-// Função para obter todos os eventos do banco de dados
-async function getAllEvents() {
-  // Implemente a lógica para obter todos os eventos do banco de dados
-  const eventsCollection = collection(db, "events");
-  const eventsQuery = query(eventsCollection, orderBy("start"));
-  const snapshot = await getDocs(eventsQuery);
-  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-}
-
-// Função para excluir um evento
-async function deleteEvent(eventId) {
-  // Implemente a lógica para excluir um evento do banco de dados
-  const eventRef = doc(db, "events", eventId);
-  await deleteDoc(eventRef);
-}
+// Função para deletar um evento
+const deleteEvent = async (eventId) => {
+    try {
+        await deleteDoc(doc(eventsCollection, eventId));
+        alert("Evento excluído com sucesso!");
+    } catch (error) {
+        console.error("Erro ao excluir evento: ", error);
+    }
+};
 
 // Função para atualizar um evento
-async function updateEvent(eventId, updatedData) {
-  // Implemente a lógica para atualizar um evento no banco de dados
-  const eventRef = doc(db, "events", eventId);
-  await updateDoc(eventRef, updatedData);
-}
+const updateEvent = async (eventId, title, start, end) => {
+    try {
+        await updateDoc(doc(eventsCollection, eventId), {
+            title: title,
+            start: start,
+            end: end,
+        });
+        alert("Evento atualizado com sucesso!");
+        closeModal();
+    } catch (error) {
+        console.error("Erro ao atualizar evento: ", error);
+    }
+};
 
-// Função para abrir o modal de edição de evento
-function openEditEventModal(eventId) {
-  // Implemente a lógica para abrir o modal de edição com base no eventId
-  const event = // Obtenha os detalhes do evento com base no eventId
-  // Preencha o modal com os detalhes do evento
-  // Abra o modal
-}
+// Função para pesquisar eventos
+const searchEvents = async () => {
+    const events = await getEvents();
+    const searchTerm = prompt("Digite um termo de pesquisa:");
+    const filteredEvents = events.filter((event) => event.title.includes(searchTerm));
+    console.log("Eventos encontrados: ", filteredEvents);
+};
 
-// Função para fechar o modal de edição de evento
-function closeModal() {
-  // Implemente a lógica para fechar o modal
-}
+// Função para ir para a página principal
+const goToIndex = () => {
+    window.location.href = "index.html";
+};
 
 // Função para abrir o modal de criação de evento
-function openCreateEventModal() {
-  // Implemente a lógica para abrir o modal de criação
-}
+const openCreateEventModal = () => {
+    openModal();
+    document.getElementById("edit-event-form").reset();
+    document.getElementById("edit-event-form").onsubmit = function (event) {
+        event.preventDefault();
+        const title = document.getElementById("event-title").value;
+        const start = document.getElementById("event-start").value;
+        const end = document.getElementById("event-end").value;
+        createEvent(title, start, end);
+    };
+};
 
-// Função para abrir o modal de pesquisa de eventos
-function openSearchEventModal() {
-  // Implemente a lógica para abrir o modal de pesquisa
-}
+// Função para abrir o modal de edição de evento
+const openEditEventModal = () => {
+    const eventId = prompt("Digite o ID do evento que deseja editar:");
+    const events = getEvents();
+    const eventToEdit = events.find((event) => event.id === eventId);
+    if (eventToEdit) {
+        openModal();
+        document.getElementById("event-title").value = eventToEdit.title;
+        document.getElementById("event-start").value = eventToEdit.start;
+        document.getElementById("event-end").value = eventToEdit.end;
+        document.getElementById("edit-event-form").onsubmit = function (event) {
+            event.preventDefault();
+            const title = document.getElementById("event-title").value;
+            const start = document.getElementById("event-start").value;
+            const end = document.getElementById("event-end").value;
+            updateEvent(eventId, title, start, end);
+        };
+    } else {
+        alert("Evento não encontrado!");
+    }
+};
 
-// Função para retornar à página principal
-function returnToMainPage() {
-  // Redirecionar para o index ou a página principal desejada
-  window.location.href = "../index.html";
-}
+// Função para abrir o modal
+const openModal = () => {
+    document.getElementById("edit-event-modal").style.display = "block";
+};
 
-// (Restante do seu código)
+// Função para fechar o modal
+const closeModal = () => {
+    document.getElementById("edit-event-modal").style.display = "none";
+};
+
+// Inicialização do calendário
+const calendar = new FullCalendar.Calendar(document.getElementById("calendar"), {
+    initialView: "dayGridMonth",
+    events: getEvents,
+    eventClick: (info) => {
+        const eventDetails = document.getElementById("event-details");
+        eventDetails.innerHTML = `<h2>${info.event.title}</h2>
+        <p><strong>Data de Início:</strong> ${info.event.start.toLocaleString()}</p>
+        <p><strong>Data de Término:</strong> ${info.event.end.toLocaleString()}</p>`;
+        eventDetails.style.display = "block";
+    },
+});
+
+// Renderização do calendário
+calendar.render();
