5a6d8202814fad961f2add025c94a4e90c1bb95b
wilsonf1996 <152716195+wilsonf1996@users.noreply.github.com>
2023-12-02 21:52:36 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,4 +9,4 @@ const firebaseConfig = {
   storageBucket: "agenda-6accc.appspot.com",
   messagingSenderId: "794262176773",
   appId: "1:794262176773:web:9bcd82e5cffd858adc086c",
-  measurementId: "G
+  measurementId: "G-QS6WRF6Y6L"
